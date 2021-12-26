dbc45ebda07201953e22cdfc5500438017301b20
Jeffrey Tocco <jnt369@gmail.com>
2021-12-25 21:57:55 -0500

    Chapter 2 changes

--- 03_02/script.js
+++ 03_02/script.js
@@ -2,21 +2,22 @@
  * Create a Backpack object.
  */
 
-const backpack = {
-  name: "Everyday Backpack",
+const Backpack = {
+  name:"Everyday Backpack",
   volume: 30,
   color: "grey",
-  pocketNum: 15,
-  strapLength: {
-    left: 26,
-    right: 26,
+  packetNum: 15,
+  strapLength:{
+    left:26,
+    right:26,
   },
-  lidOpen: false,
-  toggleLid: function (lidStatus) {
+  lidOpen:false,
+  toggleLid: function(lidStatus) {
     this.lidOpen = lidStatus;
   },
-  newStrapLength: function (lengthLeft, lengthRight) {
-    this.strapLength.left = lengthLeft;
-    this.strapLength.right = lengthRight;
-  },
-};
+
+  newStrapLength: function(lengthLeft, lengthRight){
+    this.newStrapLength.left= lengthLseft;
+    this.newStrapLength.right = lengthRight;
+  }
+}
