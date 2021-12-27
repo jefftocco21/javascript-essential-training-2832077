38df709c4a211fbe91daf01ef924ebfdcdeca371
Jeffrey Tocco <jnt369@gmail.com>
2021-12-27 13:13:11 -0500

    Playing around with querySelectors

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,8 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+var x = (document.querySelector("button"));
+
+//document.querySelectorAll("button").forEach(item => item.style.backgroundColor = "blue");
+x.style.backgroundColor = "blue";
