a9a775db5c7eebeec28959fdd372038286a57e8e
Jeff Tocco <jeff.tocco@lyonsexpress.com>
2021-12-28 14:28:06 -0500

    nested objects

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,10 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const coffee = {
+  dairy: {
+    flavor: "hazelnut",
+  },
+  sweetener: "sugar",
+  brand: "dunkin",
+};
