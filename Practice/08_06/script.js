6ec5c0b9be3594d85f4387f0b5717c0dd400bd41
Jeffrey Tocco <jnt369@gmail.com>
2021-12-27 20:41:51 -0500

    will practice arrow functions later

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -1,7 +1,19 @@
-/**
- * Practice: Pass values between functions
- *
- * - Create two functions
- * - Main function creates article element with data from object
- * - Helper function creates.
- */
+const main = document.querySelector(".maincontent");
+const notmain = document.querySelector(".notmain");
+
+function practice(param){
+    param.innerHTML = `<h1>Hello World</h1>`;
+}
+
+practice(main);
+
+const morePractice = function(param){
+    param.innerHTML = `<h1>Hello Again</h1>`;
+}
+
+practice(main);
+morePractice(notmain);
+
+
+
+
