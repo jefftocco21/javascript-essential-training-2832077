729d0a02ccd8409533185397589a108ee2cc791a
Jeff Tocco <jnt369@gmail.com>
2021-12-29 09:25:48 -0500

    updating

--- 07_03/script.js
+++ 07_03/script.js
@@ -5,28 +5,25 @@
 
 let backpackContents = ["piggy", "headlamp", "pen"];
 
-//console.log(backpackContents.join(", ")); //joining elements of array into a comma separated string
+console.log(backpackContents.join(", ")); //joining elements of array into a comma separated string
 
 backpackContents.push("bottle"); //adding items to end of array
-backpackContents.unshift("pencil") //adding items to front of array
+backpackContents.unshift("pencil"); //adding items to front of array
 backpackContents.shift(); //remove first item
 backpackContents.pop(); //remove last item
 console.log(backpackContents);
 
-
-
-backpackContents.forEach(function(item) {
-    item = `<li>${item}</li>`;
-    console.log(item);
+backpackContents.forEach(function (item) {
+  item = `<li>${item}</li>`;
+  console.log(item);
 });
 
 console.log(backpackContents);
 
-
-let longItems = backpackContents.find(function (item){
-    if(item.length >= 6){
-        return item;
-    }
+let longItems = backpackContents.find(function (item) {
+  if (item.length >= 6) {
+    return item;
+  }
 });
-console.log('longItems:', longItems);
+console.log("longItems:", longItems);
 console.log(backpackContents);
