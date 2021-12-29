8d81cc02e7efc4acbaf64bbce16f04e46a42bb08
Jeff Tocco <jnt369@gmail.com>
2021-12-29 10:43:45 -0500

    retyping arrow functions

--- 08_04/script.js
+++ 08_04/script.js
@@ -13,12 +13,12 @@ const greenPack = {
 const addPack = (currentPack) => {
   const newArticle = document.createElement("article");
   newArticle.innerHTML = `
-    <h1>${currentPack.name}</h1>
-    <ul>
-      <li>Volume: ${currentPack.volume}</li>
-      <li>Color: ${currentPack.color}</li>
-      <li>Number of pockets: ${currentPack.pocketNum}</li>
-    </ul>
+  <h1>${currentPack.name}</h1>
+  <ul>
+    <li>Volume: ${currentPack.volume}</li>
+    <li>Color: ${currentPack.color}</li>
+    <li>Number of pockets: ${currentPack.pocketNum}</li>
+  </ul>  
   `;
   return newArticle;
 };
