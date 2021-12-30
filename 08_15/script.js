d9c9c456dad1fe85917beaebb30c3e66bfe6c865
Jeff Tocco <jnt369@gmail.com>
2021-12-30 09:17:20 -0500

    array mapping more practice

--- 08_15/script.js
+++ 08_15/script.js
@@ -6,19 +6,16 @@
 const stuff = ["piggy", "headlamp", "pen", "pencil", "eraser", "water bottle"];
 
 const article = document.querySelector("article");
-let stuffList = document.createElement("ul");
+const stuffList = document.createElement("ul");
 
-// forEach() array method
-const stuffItems = stuff.map((item) => {
+const liList = stuff.map((item) => {
   let listItem = document.createElement("li");
   listItem.innerHTML = item;
   return listItem;
 });
 
-stuffItems.forEach((item) => {
+liList.forEach((item) => {
   stuffList.append(item);
 });
 
-console.log("stuff: ", stuff);
-console.log("stuffItems: ", stuffItems);
 article.append(stuffList);
